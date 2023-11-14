8fcb118deedabb5d1764dee935f5b9c878d20708
raulduha <80332171+raulduha@users.noreply.github.com>
2023-11-14 02:56:24 -0300

    tabla actualizada

--- frontend/src/components/lockerStatus/LockerStatus.js
+++ frontend/src/components/lockerStatus/LockerStatus.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import AWS from 'aws-sdk';
 import './LockerStatus.css';
 
@@ -17,7 +18,7 @@ const LockerStatus = () => {
             secretAccessKey: secretAccessKey
         })
     });
-    let lockerState; // Define lockerState outside the callback
+    let lockerState;
     const iotHandler = new AWS.IotData({ endpoint: awsEndpoint });
 
     useEffect(() => {
@@ -29,31 +30,66 @@ const LockerStatus = () => {
             if (err) {
                 console.error('Error al obtener el estado de los lockers desde AWS IoT:', err);
             } else {
-                const lockerState = JSON.parse(data.payload);
-                console.log('Locker State:', lockerState);
-                const lockersState = Object.keys(lockerState.state.reported.lockers).map(lockerId => ({
-                    id: lockerId,
-                    lock: lockerState.state.reported.lockers[lockerId].lock,
-                    door: lockerState.state.reported.lockers[lockerId].door,
-                    content: lockerState.state.reported.lockers[lockerId].content
-                }));
+                lockerState = JSON.parse(data.payload);
+                const lockersState = Object.keys(lockerState.state.reported.lockers).map(lockerId => {
+                    const localId = parseInt(lockerId.replace('l', '')); // Convierte el ID de AWS a ID local
+                    return {
+                        id: localId,
+                        lock: lockerState.state.reported.lockers[lockerId].lock,
+                        door: lockerState.state.reported.lockers[lockerId].door,
+                        content: lockerState.state.reported.lockers[lockerId].content
+                    };
+                });
 
                 setLockers(lockersState);
             }
         });
     }, []);
 
+    useEffect(() => {
+        if (lockerState) {
+            axios.get('http://127.0.0.1:8000/')
+                .then(response => {
+                    const updatedLockers = lockers.map(locker => {
+                        const matchingLocker = response.data.find(item => item.locker_id === locker.id);
+                        return {
+                            ...locker,
+                            client_email: matchingLocker ? matchingLocker.client_email : 'N/A',
+                            operator_email: matchingLocker ? matchingLocker.operator_email : 'N/A',
+                            locker_status: matchingLocker ? 'Disponible' : 'N/A'
+                        };
+                    });
+                    setLockers(updatedLockers);
+                })
+                .catch(error => {
+                    console.error('Error al obtener datos desde el servidor:', error);
+                });
+        }
+    }, [lockerState]);
+
     return (
         <div className="containerCentered">
             <div className="lockerStatus">
                 <h3>Estado de los Lockers</h3>
                 <ul className="lockerList">
+                    <li key="header" className="lockerItem tableHeader">
+                        <span>Locker ID</span>
+                        <span>Estado Candado</span>
+                        <span>Estado Puerta</span>
+                        <span>Contenido</span>
+                        <span>Estado</span>
+                        <span>Cliente Email</span>
+                        <span>Operador Email</span>
+                    </li>
                     {lockers.map(locker => (
                         <li key={locker.id} className="lockerItem">
-                            <span>Locker #{locker.id}:</span>
-                            <span>Estado del Candado: {locker.lock}</span>
-                            <span>Estado de la Puerta: {locker.door}</span>
-                            <span>Contenido: {locker.content}</span>
+                            <span>{locker.id}</span>
+                            <span>{locker.lock}</span>
+                            <span>{locker.door}</span>
+                            <span>{locker.content}</span>
+                            <span>{locker.locker_status}</span>
+                            <span>{locker.client_email}</span>
+                            <span>{locker.operator_email}</span>
                         </li>
                     ))}
                 </ul>
