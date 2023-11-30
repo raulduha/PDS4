50ef631f54357d6fbeab92670a2f13808db1edf7
Tomaspoch1 <tpoch@miuandes.cl>
2023-11-30 05:21:16 -0300

    Lockers en estaciones

--- frontend/src/components/station/StationView.js
+++ frontend/src/components/station/StationView.js
@@ -4,14 +4,17 @@ import { useParams } from 'react-router-dom';
 
 const StationView = () => {
   const [stationDetails, setStationDetails] = useState({});
+  const [lockers, setLockers] = useState([]);
   const [loading, setLoading] = useState(true);
   const { station_id } = useParams();
 
   useEffect(() => {
+    // Llamada a la API para obtener los detalles de la estación
     axios.get(`http://127.0.0.1:8000/stations/${station_id}/`)
       .then(response => {
         console.log(response.data); // Agregamos un log para verificar la respuesta del servidor
         setStationDetails(response.data.station);
+        setLockers(response.data.lockers); // Actualizamos el estado con los lockers
         setLoading(false);
       })
       .catch(error => {
@@ -30,7 +33,16 @@ const StationView = () => {
       <p><strong>ID:</strong> {stationDetails.id}</p>
       <p><strong>Name:</strong> {stationDetails.name}</p>
       <p><strong>Address:</strong> {stationDetails.address}</p>
-      {/* Puedes mostrar más detalles según tus necesidades */}
+
+      <h3>Lockers</h3>
+      <ul>
+        {lockers.map(locker => (
+          <li key={locker.id}>
+            <p><strong>ID:</strong> {locker.id}</p>
+            {/* Mostrar más detalles del locker según tus necesidades */}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
