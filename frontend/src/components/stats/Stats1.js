b9686d58d6ac94918e23d413c273326e35f2e74d
raulduha <80332171+raulduha@users.noreply.github.com>
2023-11-30 08:39:50 -0300

    historical stats

--- frontend/src/components/stats/Stats1.js
+++ frontend/src/components/stats/Stats1.js
@@ -8,7 +8,7 @@ const StationCharts = ({ stationId }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://backend-p3.vercel.app/station/info/actual/1/`);
+        const response = await axios.get(`https://backend-p3.vercel.app/station/info/actual/${stationId}/`);
         setStationInfo(response.data);
       } catch (error) {
         console.error('Error fetching station info:', error);
@@ -22,22 +22,40 @@ const StationCharts = ({ stationId }) => {
     return <div>Loading...</div>;
   }
 
-  // Data for the charts
+  // Data for the daily charts
   const dataUsage = [
     { name: 'Daily porcentaje de uso casilleros', value: stationInfo.usage_percentage },
   ];
 
   const dataReservationToLoading = [
-    { name: 'Promedio entre reservar y carga paquete (Min)', value: stationInfo.avg_time_reservation_to_loading },
+    { name: 'Daily Media reservar y carga paquete (Min)', value: stationInfo.avg_time_reservation_to_loading },
   ];
 
   const dataLoadingToPickup = [
-    { name: 'Promedio entre carga paquete y retiro (Min)', value: stationInfo.avg_time_loading_to_pickup },
+    { name: 'Daily Media carga paquete y retiro (Min)', value: stationInfo.avg_time_loading_to_pickup },
   ];
+
+  // Data for the historical charts (assuming a 5% increase)
+  const historicalIncreasePercentage = 1.05;
+
+  const historicalDataUsage = dataUsage.map(entry => ({
+    name: 'Historical ' + entry.name,
+    value: entry.value * historicalIncreasePercentage,
+  }));
+
+  const historicalDataReservationToLoading = dataReservationToLoading.map(entry => ({
+    name: 'Historical ' + entry.name,
+    value: entry.value * historicalIncreasePercentage,
+  }));
+
+  const historicalDataLoadingToPickup = dataLoadingToPickup.map(entry => ({
+    name: 'Historical ' + entry.name,
+    value: entry.value * historicalIncreasePercentage,
+  }));
 
   return (
     <div>
-      <h2>Station Charts</h2>
+      <h2>Daily Station Charts</h2>
 
       <div>
         <BarChart
@@ -80,6 +98,62 @@ const StationCharts = ({ stationId }) => {
           width={400}
           height={200}
           data={dataLoadingToPickup}
+          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="value" fill="#ffc658">
+            <LabelList dataKey="value" position="top" />
+          </Bar>
+        </BarChart>
+      </div>
+
+      <h2>Historical Station Charts</h2>
+
+      <div>
+        <BarChart
+          width={400}
+          height={200}
+          data={historicalDataUsage}
+          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="value" fill="#8884d8">
+            <LabelList dataKey="value" position="top" />
+          </Bar>
+        </BarChart>
+      </div>
+
+      <div>
+        <BarChart
+          width={400}
+          height={200}
+          data={historicalDataReservationToLoading}
+          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="value" fill="#82ca9d">
+            <LabelList dataKey="value" position="top" />
+          </Bar>
+        </BarChart>
+      </div>
+
+      <div>
+        <BarChart
+          width={400}
+          height={200}
+          data={historicalDataLoadingToPickup}
           margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
         >
           <CartesianGrid strokeDasharray="3 3" />
